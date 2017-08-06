e33cdf89730804e0d2ea9ade12db20254c1b08fe
Hamza AID <hamza@avito.ma>
2017-08-06 17:35:40 +0100

    Move some config to ENV variables

--- app/server/config.js
+++ app/server/config.js
@@ -1,6 +1,14 @@
 /* jshint node:true */
 'use strict';
 
+var slack = process.env.SLACK_TOKEN;
+var youtube = process.env.YOUTUBE_KEY;
+
+if(!(slack) || !(youtube)) {
+    console.error("Slack Bot Token or Youtube Server Key not set");
+    throw new Error("SLACK OR YOUTUBE KEY MISSING");
+}
+
 const rc = require('rc');
 
 const DEFAULTS = {
@@ -12,15 +20,15 @@ const DEFAULTS = {
         client: __dirname + '/../../_public'
     },
     slack: {
-        token: undefined
+        token: slack
     },
     youtube: {
         /**
          * this is an ISO 3166-1 alpha-2 country code
          * <http://www.iso.org/iso/country_codes/iso_3166_code_lists/country_names_and_code_elements.htm>
          */
-        region: 'PT',
-        key: undefined
+        region: 'MA',
+        key: youtube
     },
     playlist: {
         minDuration: 0,
